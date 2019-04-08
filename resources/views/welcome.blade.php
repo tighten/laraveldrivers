d032a31d87be617de60bc46e76e456eb504e9931
Matt Stauffer <matt@tighten.co>
2019-04-08 16:56:21 -0400

    Hack in mobile tabs view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,22 @@
     <div class="border-b flex items-center justify-center mb-8">
         <h1 class="font-sans py-4 text-5xl">Laravel Drivers</h1>
     </div>
-    <div class="container mx-auto">
+    <div class="container mx-4 lg:mx-auto">
+        @php
+            $activeType = Request::segments(0)[0];
+        @endphp
         <ul class="border-b hidden list-reset md:flex">
-            @php
-                $activeType = Request::segments(0)[0];
-            @endphp
             @foreach ($types as $eachtype)
             <li class="-mb-px mr-1">
                 <a class="bg-white inline-block px-4 py-2 hover:text-blue-darker{{ $activeType == $eachtype->name ? ' border-l border-r border-t font-semibold no-underline rounded-t text-blue-dark' : ' border-b text-blue' }}" href="/{{ $eachtype->name }}">{{ ucwords($eachtype->name) }}</a>
+            </li>
+            @endforeach
+        </ul>
+        <span class="font-semibold md:hidden">Types</span>
+        <ul class="md:hidden">
+            @foreach ($types as $eachtype)
+            <li class="-mb-px mr-1">
+                <a class="hover:text-blue-darker{{ $activeType == $eachtype->name ? ' font-semibold text-blue-dark' : ' text-blue' }}" href="/{{ $eachtype->name }}">{{ ucwords($eachtype->name) }}</a>
             </li>
             @endforeach
         </ul>
