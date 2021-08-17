1ef6fe22e475f7946e803046867617cb2a39cdd7
Rissa Jackson <57572484+rissajackson@users.noreply.github.com>
2021-08-17 16:54:36 -0500

    Shift updates to Laravel 8.0

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 let mix = require('laravel-mix')
 require('laravel-mix-purgecss')
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
   .postCss('resources/css/app.css', 'public/css')
   .options({
     postCss: [
@@ -10,7 +10,7 @@ mix.js('resources/js/app.js', 'public/js')
       require('postcss-nesting')(),
     ]
   })
-  .purgeCss()
+  .purgeCss();
 
 if (mix.inProduction()) {
   mix.version()
